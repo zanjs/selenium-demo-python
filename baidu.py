dd640210f33af4fd2a01e72edd2c665709429d1a
zanjs <root@zanjs.com>
2017-06-16 10:13:49 +0800

    :cat: init

---
+++ baidu.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+import time
+from selenium import webdriver
+
+driver = webdriver.Firefox()  # 打开火狐浏览器
+
+driver.get('http://www.baidu.com')  # 打开百度界面
+
+driver.find_element_by_id('kw').send_keys('zanjs')  # 在搜索框内输入想要搜索内容
+
+time.sleep(2)  # 浏览器加载需要时间
+
+driver.find_element_by_id('su').click()  # 搜索完成
